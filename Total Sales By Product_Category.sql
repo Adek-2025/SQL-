a2c43df5f9600e4597c9b9ce6e68a05cc8e23923
Adekola Adenle <adekolaadenle@gmail.com>
2025-11-02 17:26:57 +0000

    Add files via upload

---
+++ Total Sales By Product_Category.sql
@@ -0,0 +1,25 @@
+use [Global Superstore]
+Create Table Superstore(
+RowID int, 
+OrderID int, 
+Order_Date numeric, 
+Ship_Date numeric, 
+CustomerID int, 
+Customer_Name varchar (550), 
+Segment varchar(550), 
+Country varchar(550), 
+City varchar(550), 
+State varchar(550), 
+Postal_Code numeric, 
+Region varchar(550), 
+ProductID varchar(550),
+Category varchar(550),
+Sub_Category varchar(550),
+Product_Name varchar(550),
+Sales decimal(38,2),
+Quantity int,
+Discount decimal(38,2),
+Profit decimal(38,2)
+);
+
+
